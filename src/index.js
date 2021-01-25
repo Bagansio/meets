ec224f3c860a82d3f40e9f3cf22d9ea7b1fdd711
Bagansio <alexmoaleal.5@gmail.com>
2021-01-26 00:05:01 +0100

    Functional google login, but it doesnt save it in a db (26/01/21)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const passport = require('passport');
 const path = require('path');
-
 const app = express();
+const cookieSession = require('cookie-session')
+const bodyParser = require('body-parser')
 
-
+require('./passport-setup');
 // settings
 app.set('port',8000);
 app.set('view engine','ejs');                   //we set ejs to 'compile' ours html
 app.engine('html',require('ejs').renderFile);   //we 'compile' html files as ejs
 app.set('views',path.join(__dirname,'views'));
+
+    //parse application/json
+app.use(bodyParser.json());
+
+//google-login
+//                  Before global-deploy look this
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2']
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
 //middlewares
 
 
