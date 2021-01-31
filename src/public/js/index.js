61b0066dfb173e29aa1e8406d7a89a4d78795657
Bagansio <alexmoaleal.5@gmail.com>
2021-01-31 05:34:03 +0100

    It should be added before -some deleted files- (31/01/21)

--- src/public/js/index.js
+++
@@ -1,8 +0,0 @@
-let logged = <%= title%>;
-
-$(document).ready(function(){
-    if(logged) 
-        $("#user").html("<span class=\"glyphicon glyphicon-user\"></span> User");
-    else
-        $("#user").html("<span class=\"glyphicon glyphicon-log-in\"></span> Sign Up");
-});
